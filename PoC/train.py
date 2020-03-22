c6f19df45da4e057aee12f51e3fc8ec9d0c7710b
Stefan Misanovic <stefan.misanovic@hotmail.com>
2020-03-22 19:55:42 +0100

    Adding CuDNNLSTM instead of LSTM

--- PoC/train.py
+++ PoC/train.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
-from keras.layers import Dense, LSTM
+from keras.layers import Dense, CuDNNLSTM
 import matplotlib.pyplot as plt
 
 plt.style.use('fivethirtyeight')
@@ -50,8 +50,8 @@ x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 # build LSTM model
 model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(LSTM(50, return_sequences=False))
+model.add(CuDNNLSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(CuDNNLSTM(50, return_sequences=False))
 model.add(Dense(25))
 model.add(Dense(1))
 
